59f52731512eb4722b32097305b8c98a3b96a7a7
Andrew Wiebe <andrew.wiebe@senecacollege.ca>
2019-10-26 09:30:55 -0400

    customize text for b2n and andrew wiebe

--- routes/users.js
+++ routes/users.js
@@ -28,73 +28,45 @@ const userRoutes = (app, fs) => {
             context.acquireTokenWithClientCredentials(resource, applicationId, clientSecret, function(err, tokenResponse) {
             if (err) {
                 console.log('well that didn\'t work: ' + err.stack);
+                res.send(err.name);
             } else {
                 //console.log(tokenResponse);
-/*
-                //var url = 'https://graph.windows.net/andrewtestingb2c.onmicrosoft.com/users?api-version=1.6';
-                var url= 'https://api.github.com/repos/request/request';
 
-                request.get({
-                    url: url,
-                    json: true,
+                const options = {
+                    url: 'https://graph.windows.net/andrewtestingb2c.onmicrosoft.com/users?api-version=1.6',
                     headers: {
-                        //'Authorization' : 'Bearer ' + tokenResponse.accessToken,
-                        'User-Agent': 'request'
+                        'User-Agent': 'request',
+                        'Authorization': 'Bearer ' + tokenResponse.accessToken
                     }
-                  }, (err, res, data) => {
-                    if (err) {
-                      console.log('Error:', err);
-                    } else if (res.statusCode !== 200) {
-                      console.log('Status:', res.statusCode);
-                    } else {
-                      // data is already parsed as JSON:
-                      //console.log(res.statusCode + ' ' + res.statusMessage + ' ' + data);
-                      const info = JSON.parse(body);
-                      console.log(info);
+                };
+                
+                function callback(error, response, body) {
+                    if (!error && response.statusCode == 200) {
+                    jsonres = JSON.parse(body);
+                    //console.log(jsonres);
+                    //console.log(info.stargazers_count + " Stars");
+                    //console.log(info.forks_count + " Forks");
+
+                    var arrValues = jsonres.value;
+                    var arrsignInNamres = [];
+
+                    for(var i = 0; i < arrValues.length; i++ ){
+                        if (arrValues[i].signInNames[0]) {
+                            arrsignInNamres.push(arrValues[i].signInNames[0].value);
+                            console.log(arrsignInNamres[i]);
+                        }
                     }
-                });
-                */
 
-            const options = {
-                url: 'https://graph.windows.net/andrewtestingb2c.onmicrosoft.com/users?api-version=1.6',
-                headers: {
-                  'User-Agent': 'request',
-                  'Authorization' : 'Bearer ' + tokenResponse.accessToken
-                }
-            };
-              
-            function callback(error, response, body) {
-                if (!error && response.statusCode == 200) {
-                  jsonres = JSON.parse(body);
-                  //console.log(jsonres);
-                  //console.log(info.stargazers_count + " Stars");
-                  //console.log(info.forks_count + " Forks");
 
-                var arrValues = jsonres.value;
-                var arrsignInNamres = [];
-
-                for(var i = 0; i < arrValues.length; i++ ){
-                    if (arrValues[i].signInNames[0]) {
-                        arrsignInNamres.push(arrValues[i].signInNames[0].value);
-                        console.log(arrsignInNamres[i]);
+                    //res.send(jsonres.value[1].signInNames[0].value);
+                    //res.send(arrsignInNamres);
+                    res.render('index',{names:arrsignInNamres});
+                    
                     }
                 }
-
-
-                //res.send(jsonres.value[1].signInNames[0].value);
-                //res.send(arrsignInNamres);
-                res.render('index',{names:arrsignInNamres});
                 
-                }
-            }
-              
-            request(options, callback);
-                
-
-                
+                request(options, callback);
             
-
-
             }
             });
 
