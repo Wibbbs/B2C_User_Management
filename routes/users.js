65a3db2d2ff48fba0338967e4cfef8a3061a2ec1
Andrew Wiebe <andrew.wiebe@senecacollege.ca>
2019-10-29 15:41:06 -0400

    removed data.json references

--- routes/users.js
+++ routes/users.js
@@ -86,75 +86,69 @@ var resource = '00000002-0000-0000-c000-000000000000'; // URI that identifies th
 const userRoutes = (app, fs) => {
 
     // variables
-    const dataPath = './data/users.json';
     var jsonres = ' ';
 
     // READ
-    app.get('/users', (req, res) => {
-        fs.readFile(dataPath, 'utf8', (err, data) => {
-            if (err) {
-                throw err;
-            }
-            var context = new AuthenticationContext(authorityUrl);
-            var tokenstring; 
+    app.get('/users', (req, res) => {       
+        var context = new AuthenticationContext(authorityUrl);
+        var tokenstring; 
+        
+        context.acquireTokenWithClientCredentials(resource, applicationId, clientSecret, function(err, tokenResponse) {
+        if (err) {
+            console.log('well that didn\'t work: ' + err.stack);
+            res.send(err.name);
+        } else {
+            //console.log(tokenResponse);
+
+            const options = {
+                url: 'https://graph.windows.net/andrewtestingb2c.onmicrosoft.com/users?api-version=1.6',
+                headers: {
+                    'User-Agent': 'request',
+                    'Authorization': 'Bearer ' + tokenResponse.accessToken
+                }
+            };
             
-            context.acquireTokenWithClientCredentials(resource, applicationId, clientSecret, function(err, tokenResponse) {
-            if (err) {
-                console.log('well that didn\'t work: ' + err.stack);
-                res.send(err.name);
-            } else {
-                //console.log(tokenResponse);
+            function callback(error, response, body) {
+                if (!error && response.statusCode == 200) {
+                jsonres = JSON.parse(body);
+                //console.log(jsonres);
+                //console.log(info.stargazers_count + " Stars");
+                //console.log(info.forks_count + " Forks");
 
-                const options = {
-                    url: 'https://graph.windows.net/andrewtestingb2c.onmicrosoft.com/users?api-version=1.6',
-                    headers: {
-                        'User-Agent': 'request',
-                        'Authorization': 'Bearer ' + tokenResponse.accessToken
-                    }
-                };
-                
-                function callback(error, response, body) {
-                    if (!error && response.statusCode == 200) {
-                    jsonres = JSON.parse(body);
-                    //console.log(jsonres);
-                    //console.log(info.stargazers_count + " Stars");
-                    //console.log(info.forks_count + " Forks");
+                var arrValues = jsonres.value;
+                var arrsignInNamres = [];
 
-                    var arrValues = jsonres.value;
-                    var arrsignInNamres = [];
+                for(var i = 0; i < arrValues.length; i++ ){
+                    //For Signin Names (some users domt have signinnames)
+                    /*if (arrValues[i].signInNames[0]) {
+                        arrsignInNamres.push(arrValues[i].signInNames[0].value);
+                        console.log(arrsignInNamres[i]);
+                    }*/
 
-                    for(var i = 0; i < arrValues.length; i++ ){
-                        //For Signin Names (some users domt have signinnames)
-                        /*if (arrValues[i].signInNames[0]) {
-                            arrsignInNamres.push(arrValues[i].signInNames[0].value);
-                            console.log(arrsignInNamres[i]);
-                        }*/
-
-                        //For display names
-                        if (arrValues[i].displayName) {
-                            arrsignInNamres.push(arrValues[i].displayName);
-                            console.log(arrsignInNamres[i]);
-                        }
-                    }
-
-
-                    //res.send(jsonres.value[1].signInNames[0].value);
-                    //res.send(arrsignInNamres);
-                    //res.render('listusers',{names:arrsignInNamres});
-                    
-                    //res.send(jsonTemp);
-                    res.render('userlist.pug',{data:jsonres.value,tenant:tenant})
+                    //For display names
+                    if (arrValues[i].displayName) {
+                        arrsignInNamres.push(arrValues[i].displayName);
+                        console.log(arrsignInNamres[i]);
                     }
                 }
-                
-                request(options, callback);
-            
-            }
-            });
 
-            //res.send(tokenstring);
-            //res.send(JSON.parse(data));
+
+                //res.send(jsonres.value[1].signInNames[0].value);
+                //res.send(arrsignInNamres);
+                //res.render('listusers',{names:arrsignInNamres});
+                
+                //res.send(jsonTemp);
+                res.render('userlist.pug',{data:jsonres.value,tenant:tenant})
+                }
+            }
+            
+            request(options, callback);
+        
+        }
         });
+
+        //res.send(tokenstring);
+        //res.send(JSON.parse(data));     
     });
 };
 
