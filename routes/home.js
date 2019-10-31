9df0d1c9c93b259d2d1d6945b98e0c213505db6d
Andrew Wiebe <andrew.wiebe@senecacollege.ca>
2019-10-31 10:46:35 -0400

    added dropdown and chagenotes

--- routes/home.js
+++ routes/home.js
@@ -4,6 +4,10 @@ const homeRoutes = (app, fs) => {
     app.get('/', (req, res) => {
         res.render('home.pug');
     });
+    // The Default route to handle / requests. This will render the home page.
+    app.get('/changenotes', (req, res) => {
+        res.render('changenotes.pug');
+    });
 
 }
 
